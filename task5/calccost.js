2ce3a127f39ef0338bfc38fb45ba2f1c8c8daaf6
masyuncheg <maksim.lebedev2005@mail.ru>
2024-10-12 17:12:54 +0300

    Update calccost.js

--- task5/calccost.js
+++ task5/calccost.js
@@ -1,4 +1,4 @@
-function calccost()
+window.addEventListener("DOMContentLoaded", function calccost()
 {
     let count=document.getElementsByName("count");
     let product=document.getElementsByName("product");
@@ -12,4 +12,4 @@ function calccost()
     res.innerHTML=result+" руб.";
 
     return false;
-}
+})
