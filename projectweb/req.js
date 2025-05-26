30a1d6752f10536d32bb5ba0d7f1b97ad6310e32
masyuncheg <maksim.lebedev2005@mail.ru>
2025-05-26 21:19:48 +0300

    Update req.js

--- projectweb/req.js
+++ projectweb/req.js
@@ -1,7 +1,7 @@
 document.getElementById('form').addEventListener('submit', async function(e) {
     e.preventDefault();
     const form = e.target;
-    
+    const dinBlock = document.querySelector('.din');
      const formData = new FormData(form);
 
     try {
@@ -10,14 +10,17 @@ document.getElementById('form').addEventListener('submit', async function(e) {
             headers: {'X-Requested-With': 'XMLHttpRequest'},
             body: formData,
         });
-        
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
         const data = await response.json();
         
         if (data.success) {
             // Обработка успешной отправки
             alert(data.messages.success);
             if (data.login && data.password) {
-                console.log(`Login: ${data.login}, Password: ${data.password}`);
+                dinBlock.innerHTML = '`
+                    Логин: <strong>${data.login}</strong><br>
+                    Пароль: <strong>${data.password}</strong>
+                `';
             }
         } else {
             // Показ ошибок
